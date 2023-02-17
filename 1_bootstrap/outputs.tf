3145bdcd74b51fc9cd07bcd674e4daf14851c2db
Maxime Roy <maximeroy@microsoft.com>
2023-02-17 16:05:16 -0500

    Removed unused output

--- 1_bootstrap/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group" {
-  value       = module.bootstrap_resource_group
-  description = "Output all the values of the resource group."
-}
-
-output "storage_account" {
-  value       = module.bootstrap_storage_account
-  description = "Output all the values of the storage account."
-}
