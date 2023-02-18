3f7e3796fefd77aae735d1298fa73dc004900093
Kevin <kevin.evans1@outlook.com>
2023-02-17 21:39:33 -0700

    more terraform goodness

---
+++ 2_caf_deployment/terraform/azure_vwan_deployment/main.tf
@@ -0,0 +1,42 @@
+# Dual region Azure VWan Topology Deployment
+
+# Get the current client configuration from the AzureRM provider.
+# This is used to populate the root_parent_id variable with the
+# current Tenant ID used as the ID for the "Tenant Root Group"
+# Management Group.
+
+data "azurerm_client_config" "core" {}
+
+# Declare the Azure landing zones Terraform module
+# and provide a base configuration.
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "3.0.2"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm.connectivity
+    azurerm.management   = azurerm.management
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = var.root_id
+  root_name      = var.root_name
+
+  # Connectivity Deployment Configuration
+  deploy_connectivity_resources    = var.deploy_connectivity_resources
+  subscription_id_connectivity     = var.subscription_id_connectivity
+  configure_connectivity_resources = local.configure_connectivity_resources
+
+  # Identity Deployment Configuration
+  deploy_identity_resources    = var.deploy_identity_resources
+  subscription_id_identity     = var.subscription_id_identity
+  configure_identity_resources = local.configure_identity_resources
+
+  # Managment Deployment Configuration
+  deploy_management_resources    = var.deploy_management_resources
+  subscription_id_management     = var.subscription_id_management
+  configure_management_resources = local.configure_management_resources
+
+}
---
+++ 2_caf_deployment/terraform/azure_vwan_deployment/settings.connectivity.tf
@@ -0,0 +1,161 @@
+# Configure the connectivity resources settings.
+locals {
+  configure_connectivity_resources = {
+    settings = {
+      hub_networks = []
+      vwan_hub_networks = [
+        {
+          enabled = true
+          config = {
+            address_prefix = "10.200.0.0/22"
+            location       = "northeurope"
+            sku            = ""
+            routes         = []
+            expressroute_gateway = {
+              enabled = true
+              config = {
+                scale_unit = 1
+              }
+            }
+            vpn_gateway = {
+              enabled = false
+              config = {
+                bgp_settings       = []
+                routing_preference = ""
+                scale_unit         = 1
+              }
+            }
+            azure_firewall = {
+              enabled = true
+              config = {
+                enable_dns_proxy              = true
+                dns_servers                   = []
+                sku_tier                      = "Standard"
+                base_policy_id                = ""
+                private_ip_ranges             = []
+                threat_intelligence_mode      = ""
+                threat_intelligence_allowlist = []
+                availability_zones = {
+                  zone_1 = true
+                  zone_2 = true
+                  zone_3 = true
+                }
+              }
+            }
+            spoke_virtual_network_resource_ids = []
+            enable_virtual_hub_connections     = false
+          }
+        },
+        {
+          enabled = true
+          config = {
+            address_prefix = "10.201.0.0/22"
+            location       = "westeurope"
+            sku            = ""
+            routes         = []
+            expressroute_gateway = {
+              enabled = false
+              config = {
+                scale_unit = 1
+              }
+            }
+            vpn_gateway = {
+              enabled = true
+              config = {
+                bgp_settings       = []
+                routing_preference = ""
+                scale_unit         = 1
+              }
+            }
+            azure_firewall = {
+              enabled = false
+              config = {
+                enable_dns_proxy              = true
+                dns_servers                   = []
+                sku_tier                      = "Standard"
+                base_policy_id                = ""
+                private_ip_ranges             = []
+                threat_intelligence_mode      = ""
+                threat_intelligence_allowlist = []
+                availability_zones = {
+                  zone_1 = true
+                  zone_2 = true
+                  zone_3 = true
+                }
+              }
+            }
+            spoke_virtual_network_resource_ids = []
+            enable_virtual_hub_connections     = false
+          }
+        },
+      ]
+      ddos_protection_plan = {
+        enabled = true
+        config = {
+          location = "northeurope"
+        }
+      }
+      dns = {
+        enabled = true
+        config = {
+          location = null
+          enable_private_link_by_service = {
+            azure_automation_webhook             = true
+            azure_automation_dscandhybridworker  = true
+            azure_sql_database_sqlserver         = true
+            azure_synapse_analytics_sqlserver    = true
+            azure_synapse_analytics_sql          = true
+            storage_account_blob                 = true
+            storage_account_table                = true
+            storage_account_queue                = true
+            storage_account_file                 = true
+            storage_account_web                  = true
+            azure_data_lake_file_system_gen2     = true
+            azure_cosmos_db_sql                  = true
+            azure_cosmos_db_mongodb              = true
+            azure_cosmos_db_cassandra            = true
+            azure_cosmos_db_gremlin              = true
+            azure_cosmos_db_table                = true
+            azure_database_for_postgresql_server = true
+            azure_database_for_mysql_server      = true
+            azure_database_for_mariadb_server    = true
+            azure_key_vault                      = true
+            azure_kubernetes_service_management  = true
+            azure_search_service                 = true
+            azure_container_registry             = true
+            azure_app_configuration_stores       = true
+            azure_backup                         = true
+            azure_site_recovery                  = true
+            azure_event_hubs_namespace           = true
+            azure_service_bus_namespace          = true
+            azure_iot_hub                        = true
+            azure_relay_namespace                = true
+            azure_event_grid_topic               = true
+            azure_event_grid_domain              = true
+            azure_web_apps_sites                 = true
+            azure_machine_learning_workspace     = true
+            signalr                              = true
+            azure_monitor                        = true
+            cognitive_services_account           = true
+            azure_file_sync                      = true
+            azure_data_factory                   = true
+            azure_data_factory_portal            = true
+            azure_cache_for_redis                = true
+          }
+          private_link_locations = [
+            "northeurope",
+            "westeurope",
+          ]
+          public_dns_zones                                       = []
+          private_dns_zones                                      = []
+          enable_private_dns_zone_virtual_network_link_on_hubs   = true
+          enable_private_dns_zone_virtual_network_link_on_spokes = true
+        }
+      }
+    }
+
+    location = var.connectivity_resources_location
+    tags     = var.connectivity_resources_tags
+    advanced = null
+  }
+}
